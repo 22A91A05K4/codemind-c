913b8fbe1a74d04a8c64088371d03000655d87f4
22A91A05K4 <119839797+22A91A05K4@users.noreply.github.com>
2022-12-06 10:40:33 +0530

    #include<stdio.h> int main() {     int a,i,c=0;     scanf("%d",&a);     for(i=1;i<a;i++)     {         if(a%i==0)         c=c+i;
        }
        if(c==a)
        {
            printf("PERFECT");
        }
        else if(c>a)
        {
            printf("ABUNDANT");
        }
        else
        {
            printf("DEFICIENT");
        }
    }

---
+++ PDA.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,c=0;
+    scanf("%d",&a);
+    for(i=1;i<a;i++)
+    {
+        if(a%i==0)
+        c=c+i;
+        
+    }
+    if(c==a)
+    {
+        printf("PERFECT");
+    }
+    else if(c>a)
+    {
+        printf("ABUNDANT");
+    }
+    else
+    {
+        printf("DEFICIENT");
+    }
+}
