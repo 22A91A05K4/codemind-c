fb6e3bb86517fd35a1bebbdaabf7e85ea3dc5c51
22A91A05K4 <119839797+22A91A05K4@users.noreply.github.com>
2022-12-10 23:07:46 +0530

    #include<stdio.h> int main() {     char c;     scanf("%c",&c);     if(c>='A' && c<='Z')     {         printf("uppercase alphabet");     }     else if(c>='a' && c<='z')     {         printf("lowercase alphabet");     }     else     {         printf("not an alphabet");     } }

---
+++ Character_is_Uppercase_or_Lowercase_.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if(c>='A' && c<='Z')
+    {
+        printf("uppercase alphabet");
+    }
+    else if(c>='a' && c<='z')
+    {
+        printf("lowercase alphabet");
+    }
+    else
+    {
+        printf("not an alphabet");
+    }
+}
