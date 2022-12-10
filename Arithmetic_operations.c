8c407dd479ffdabc4512d90c0346854ba141963a
22A91A05K4 <119839797+22A91A05K4@users.noreply.github.com>
2022-12-10 20:11:22 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     printf("Sum:%d ",c=a+b);     printf("Difference:%d ",c=a-b);     printf("Product:%d ",c=a*b);     printf("Quotient:%d ",c=a/b);     printf("Remainder:%d",c=a%b); }

---
+++ Arithmetic_operations.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    printf("Sum:%d
+",c=a+b);
+    printf("Difference:%d
+",c=a-b);
+    printf("Product:%d
+",c=a*b);
+    printf("Quotient:%d
+",c=a/b);
+    printf("Remainder:%d",c=a%b);
+}
