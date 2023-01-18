4071f6d076ab1401e78541069eff7552ce37a8ce
22A91A05K4 <119839797+22A91A05K4@users.noreply.github.com>
2023-01-18 12:25:32 +0530

    #include<stdio.h> int main() {     int a,b,c=0,i;     scanf("%d",&a);     for(i=2;i<=a;i++)     {         if(a%i==0 && a%1==0)         c=1;      break;     }     if(c==1)     {         printf("Not Prime");     }     else     {         printf("Prime");     } }

--- Prime.c
+++ Prime.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int a,i,c=0;
+    int a,b,c=0,i;
     scanf("%d",&a);
     for(i=2;i<=a;i++)
     {
-        if(a%i==0)
+        if(a%i==0 && a%1==0)
         c=1;
-        break;
+     break;   
     }
     if(c==1)
     {
