704c92ca0ea7df73c21113ba0bdac6973bff198e
22A91A05K4 <119839797+22A91A05K4@users.noreply.github.com>
2023-01-19 11:22:13 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);
        int a[n],i,c=0;
        for(i=0;i<n;i++)
        {
            scanf("%d",&a[i]);
        }
        for(i=0;i<n;i++)
        {
            if(i%2==0)
             c=c+a[i];
        }
        printf("%d",c);
    }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    
+    int a[n],i,c=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+         c=c+a[i];
+    }
+    printf("%d",c);
+}
